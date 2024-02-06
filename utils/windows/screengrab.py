4479cbfe30ba02e6d789ed2a67c65bced1b5f0b4
Keith Adler <talador12@gmail.com>
2024-02-05 23:18:45 -0600

    hotfix to squash

--- utils/windows/screengrab.py
+++ utils/windows/screengrab.py
@@ -17,7 +17,7 @@ def screen_record(
 
     if left_right_mode:
         # Left/Right Mode
-        return np.asarray(sct.grab(monitor))
+        return np.rot90(np.asarray(sct.grab(monitor)),0)
     # Top/Bottom Mode
     return np.rot90(np.asarray(sct.grab(monitor)))
 
